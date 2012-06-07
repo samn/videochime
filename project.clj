2a817e9e28d8f3fe4b233f9a4d65b688defa633d
Sam Neubardt <samneubardt@gmail.com>
2012-06-07 15:27:22 -0400

    Add overtone & core.match

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [overtone "0.6.0"]
+                 [org.clojure/core.match "0.2.0-alpha9"]])
