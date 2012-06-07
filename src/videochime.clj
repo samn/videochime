cdcd3727add89e4c627ecbbadb34310fd679e709
Sam Neubardt <samneubardt@gmail.com>
2012-06-07 16:24:19 -0400

    Add get-data
    Fetches and parses a response from the analytics api

--- src/videochime.clj
+++ src/videochime.clj
@@ -2,7 +2,7 @@
   (:require [clojure.core.match :refer [match]]
             [overtone.live :refer :all]
             [clj-http.client :as http]
-            [cheshire.core :refer [parse-string]]))
+            [cheshire.core :refer [parse-string] :as json]))
 
 (def ^:dynamic *account-id* 1160438696001)
 (def ^:dynamic *auth-token* "14fce7a31e4b08587081ee147")
@@ -19,6 +19,16 @@
     (str url-base *account-id* "/" (name resource) "/" id)
     (str url-base *account-id*)))
 
+(defn get-data
+  "Returns the parsed body of a request to the Analytics API.
+  A specific resource & id are optional"
+  [& [resource id]]
+  (let [headers {"Authorization" (str "Bearer " *auth-token*)}
+        url (build-url resource id)]
+    (-> 
+      (http/get url {:headers headers})
+      :body
+      (json/parse-string))))
 
 (defn -main
   "I don't do a whole lot."
