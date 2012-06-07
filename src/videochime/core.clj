a6a63f44245deec1547f6af6b22e5c355f560030
Sam Neubardt <samneubardt@gmail.com>
2012-06-07 15:20:35 -0400

    init

---
+++ src/videochime/core.clj
@@ -0,0 +1,6 @@
+(ns videochime.core)
+
+(defn -main
+  "I don't do a whole lot."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/videochime/core_test.clj
@@ -0,0 +1,7 @@
+(ns videochime.core-test
+  (:use clojure.test
+        videochime.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
