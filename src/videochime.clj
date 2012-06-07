9b0e228d4bd0b903b06fdad03812a7b06c11a4bb
Sam Neubardt <samneubardt@gmail.com>
2012-06-07 15:42:31 -0400

    Add some config params & build-url
    build-url constructs a url for a request to the analytics api

--- src/videochime.clj
+++ src/videochime.clj
@@ -3,7 +3,18 @@
             [overtone.live :refer :all]
             [clj-http.client :as http]))
 
+(def ^:dynamic *account-id* 1160438696001)
+(def ^:dynamic *auth-token* "14fce7a31e4b08587081ee147")
+(def url-base "http: //data.brightcove.com/analytics-api/data/videocloud/account/")
 
+(defn build-url
+  "Construct a url for a request to the analytics api.
+  Uses *account-id*. Params are optional, but if included the
+  first param should be the resource to fetch
+  info for (video or player). The second arg should be the id
+  of the resource being fetched."
+  [& [resource id]]
+  (str url-base *account-id* "/" (name resource) "/" id))
 
 (defn -main
   "I don't do a whole lot."
