a2efe97dd2142614245f19218f6274afe8b1f0bd
Sam Neubardt <samneubardt@gmail.com>
2012-06-19 17:31:38 -0400

    Add a main ns

--- project.clj
+++ project.clj
@@ -3,10 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  ;:main videochime
+  :main videochime
+  :aot [videochime]
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [overtone "0.6.0"]
                  [org.clojure/core.match "0.2.0-alpha9"]
+                 [overtone "0.6.0"]
                  [clj-http "0.4.3"]
                  [cheshire "4.0.0"]
                  [midje "1.4.0" :scope "test"]])
